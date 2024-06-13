06624bf1070838914053e034d41f47775a87aeb7
PMSIS <PMSIS@DESKTOP-8HOGNBF>
2024-06-13 17:46:17 +0530

    Add project files.

---
+++ EmployeeManagementSysTeam/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeesManagementSysTeam.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSysTeam.Context
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions) : base(contextOptions)
+		{
+
+		}
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
---
+++ EmployeeManagementSysTeam/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeesManagementSysTeam.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string MiddleName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string EmailAddress { get; set; }
+
+		public int PhoneNumber { get; set; }
+	}
+}
